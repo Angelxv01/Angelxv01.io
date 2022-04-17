5e21152937904158a316334a8d0589961badcf7d
angelxv01 <angeloxv@outlook.it>
2022-04-17 19:18:50 +0200

    refresh project with tailwind

--- static/js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#navbar-toggler').addEventListener('click', () => {
-        const navBar = document.querySelector('#navbarNavAltMarkup');
-        !navBar.classList.contains('show')
-            ? navBar.classList.add('show')
-            : navBar.classList.remove('show')
-    })
-})
